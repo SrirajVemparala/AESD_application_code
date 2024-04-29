7f4fff66ff726c9631a0c04b2db69b2d083a1a81
Isha Sharma <123535278+ishassharmaa@users.noreply.github.com>
2024-04-28 18:22:55 -0600

    Update server-start-stop.sh

--- socket/server-start-stop.sh
+++ socket/server-start-stop.sh
@@ -8,7 +8,7 @@
 case "$1" in
     start)
         echo "Starting aesdsocket server"
-	start-stop-daemon -S -n server -a /usr/bin/aesdsocket -- -d
+	start-stop-daemon -S -n server -a /usr/bin/server -- -d
         ;;
     stop)
         echo "Stopping aesdsocket server"
